54a6cccfdbff416a6c9f229cb177f38ed25f4080
lychie <lychie@yeah.net>
2014-07-18 06:56:18 +0800

    add tips info for jelly install script

--- jelly-install.bat
+++ jelly-install.bat
@@ -5,15 +5,35 @@
 	COLOR 3E
 	
 	echo [INFO] ------------------------------------------------------------------------
+	echo [INFO]  
 	echo [INFO] Installing jelly, this may take a few minutes, please wait . . .
+	echo [INFO]  
 	echo [INFO] ------------------------------------------------------------------------
 
 	cd %~dp0
 	
 	call mvn clean install -Dmaven.test.skip=true
+	
+	rd /q /s bin
+	
+	md bin
+	
+	xcopy core\target\jelly*.GA.jar bin /Y
+	
+	rd /q /s javadoc
+	
+	md javadoc
+	
+	xcopy core\target\jelly*.GA-javadoc.jar javadoc /Y
+	
+	ren javadoc\jelly*.GA-javadoc.jar *.zip
 
+	echo [INFO]  
 	echo [INFO] ------------------------------------------------------------------------
-	echo [INFO] Finish
+	echo [INFO]  
+	echo [INFO] Finish! See javadoc directory use api, and jar is in the bin directory.
+	echo [INFO]  
 	echo [INFO] ------------------------------------------------------------------------
+	echo [INFO]  
 
 pause
